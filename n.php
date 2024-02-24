e096cd4466e850d8c2bdd94ded981a0f7996ce8d
Keshav8822 <109322951+Keshav8822@users.noreply.github.com>
2024-02-24 12:58:11 +0530

    Update n.php

--- n.php
+++ n.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $latitude = $_POST["latitude"];
     $longitude = $_POST["longitude"];
     
-    $to = "your@email.com";
+    $to = "keshu5232@gmail.com";
     $subject = "User Location";
     $message = "Latitude: " . $latitude . "\r\n" . "Longitude: " . $longitude;
     $headers = "From: webmaster@example.com" . "\r\n" .
